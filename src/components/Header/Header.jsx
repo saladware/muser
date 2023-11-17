d03293300c1b5fd2673de3ea06b4da90fb3f507a
saladware <saladware46@gmail.com>
2023-11-18 02:39:21 +0300

    stage2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,23 +2,31 @@ import React from 'react';
 import classes from "./Header.module.css"
 
 const Header = () => {
-    return (
-        <header>
-            <div id="logo">
-                <img src="" alt=""/>
-                <span>nom</span>
-            </div>
-            <nav>
-                <ul>
-                    <li><a href="">курсы</a></li>
-                    <li><a href="">преимущества</a></li>
-                    <li><a href="">особенности</a></li>
-                    <li><a href="">о нас</a></li>
-                    <li><a href="">обратная связь</a></li>
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header>
+
+      <div id={classes.logo}>
+        <a href="/home">
+          <div>
+            <img src="/logo.svg" alt="" />
+            <span>Nom</span>
+          </div>
+        </a>
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#s3">курсы</a></li>
+          <li><a href="">преимущества</a></li>
+          <li><a href="">особенности</a></li>
+          <li><a href="">о нас</a></li>
+          <li><a href="">обратная связь</a></li>
+        </ul>
+      </nav>
+      <div id={classes.auth}>
+        <a href="/login">войти</a>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
